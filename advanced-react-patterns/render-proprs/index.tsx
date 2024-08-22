d45d8fab3939b63f84fca25073ac4409f3863169
ildar <ametov.ildar@gmail.com>
2024-08-22 23:43:08 +0300

    [INTERVIEW-DUMP-1] - Render props pattern

---
+++ advanced-react-patterns/render-proprs/index.tsx
@@ -0,0 +1,30 @@
+import { type ReactNode, useCallback } from 'react'
+
+interface Props<T> {
+  items: Array<T>
+  renderProps: (item: T) => ReactNode
+}
+
+const Grid = <T,>({ items, renderProps }: Props<T>) => {
+  const renderItems = useCallback(
+    (_items: typeof items) => {
+      return _items.map((item) => renderProps(item))
+    },
+    [renderProps]
+  )
+
+  return <div className="grid">{items?.length ? renderItems(items) : <span>Список пуст</span>}</div>
+}
+
+const ITEMS = [
+  { title: 'Title 1', href: 'Title 1' },
+  { title: 'Title 2', href: 'Title 2' }
+]
+
+const SomeComponent = () => (
+  <>
+    <h1>Render props pattern</h1>
+
+    <Grid items={ITEMS} renderProps={(item) => <a href={item.href}>{item.title}</a>} />
+  </>
+)
